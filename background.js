c7f18cd1eac775c1ede0788827d6c156837d552c
BrainyShadow <seb997a@hotmail.com>
2022-05-09 20:54:04 -0400

    Updated permissions
    Update permissions and deleted unnecessary code.

--- background.js
+++ background.js
@@ -1,29 +1,4 @@
-function fallbackCopyTextToClipboard(text) {
-  var textArea = document.createElement("textarea");
-  textArea.value = text;
-
-  // Avoid scrolling to bottom
-  textArea.style.top = "0";
-  textArea.style.left = "0";
-  textArea.style.position = "fixed";
-
-  document.body.appendChild(textArea);
-  textArea.focus();
-  textArea.select();
-
-  try {
-    var successful = document.execCommand("copy");
-    var msg = successful ? "successful" : "unsuccessful";
-    console.log("Fallback: Copying text command was " + msg);
-  } catch (err) {
-    console.error("Fallback: Oops, unable to copy", err);
-  }
-
-  document.body.removeChild(textArea);
-}
-
 chrome.action.onClicked.addListener(async function (tab) {
-  fallbackCopyTextToClipboard("TEST");
 
   chrome.tabs.captureVisibleTab(
     null,
--- content_script.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.requested == "createDiv") {
-    //Code to create the div
-    sendResponse({ confirmation: "Successfully created div" });
-  }
-});
